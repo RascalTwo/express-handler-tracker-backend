18b8be8fcc4956ee03f34e61216c398f3a5c8cbb
Rascal_Two <therealrascaltwo@gmail.com>
2022-09-14 19:04:22 -0400

    Increase parsing limit

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ connectDB();
 app.set("view engine", "ejs");
 app.use(cors())
 //Body Parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: '5mb' }));
+app.use(express.json({ limit: '5mb' }));
 
 //Logging
 app.use(logger("dev"));
